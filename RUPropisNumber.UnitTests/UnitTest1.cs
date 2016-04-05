d9f51a09fc4b8ec6cd922f062e537357d52b3ac2
Dmitry Shevyakov <stormbringer2011@gmail.com>
2016-04-05 12:21:20 +0400

    added tests

--- RUPropisNumber.UnitTests/UnitTest1.cs
+++ RUPropisNumber.UnitTests/UnitTest1.cs
@@ -11,8 +11,39 @@ namespace RUPropisNumber.UnitTests
         public void TestMethod1()
         {
             string res = CurPhrase(103.64M);
+            Assert.AreEqual(res, "Сто три рубля 64 копейки");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string res = CurPhrase(103236.61M);
+            Assert.AreEqual(res, "Сто три тысячи двести тридцать шесть рублей 61 копейка");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string res = Propis.NumPhrase(103456,true);
+            Assert.AreEqual(res, "Сто три тысячи четыреста пятьдесят шесть");
         }
 
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string res = Propis.NumPhrase(103456,true);
+            Assert.AreEqual(res, "Сто три тысячи четыреста пятьдесят шесть");
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string res = Propis.NumPhrase(42,true);
+            Assert.AreEqual(res, "Сорок два");
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string res = Propis.NumPhrase(42,false);
+            Assert.AreEqual(res, "Сорок две");
+        }
         private string CurPhrase(decimal sum)
         {
             return Propis.CurPhrase(sum, PropisList.Big643, PropisList.Small643);
