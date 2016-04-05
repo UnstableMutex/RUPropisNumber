4369aed54d22138826f6f303a6e357d976c21a9e
Dmitry Shevyakov <stormbringer2011@gmail.com>
2016-04-05 14:05:06 +0400

    removed refs

--- RUPropisNumber/Implementations/RUB.cs
+++ RUPropisNumber/Implementations/RUB.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
 using RUPropisNumber.MEFAttributes;
 
 namespace RUPropisNumber.Implementations
